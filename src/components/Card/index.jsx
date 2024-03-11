01b4b6ead8c772b1852683d5fde60fc41e02f40e
MeddyBalont <Meddy.Balont@hotmail.com>
2024-03-11 23:47:35 +0100

    API

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,61 +4,61 @@ import DefaultPicture from '../../assets/profile.png'
 import colors from '../../utils/style/colors'
 
 const CardLabel = styled.span`
-  color: #5843e4;
-  font-size: 22px;
-  font-weight: bold;
-  padding-left: 15px;
+      color: #5843e4;
+      font-size: 22px;
+      font-weight: bold;
+      padding-left: 15px;
 `
 
 const CardTitle = styled.span`
       color: black;
       font-size: 22px;
       font-weight: normal;
-      align-items: center;
+      text-align: center;
 `
 
 const CardImage = styled.img`
-  height: 150px;
-  width: 150px;
-  align-self: center;
-  border-radius: 50%;
+      height: 150px;
+      width: 150px;
+      align-self: center;
+      border-radius: 50%;
 `
 
 const CardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  padding: 15px;
-  background-color: ${colors.backgroundLight};
-  border-radius: 30px;
-  width: 300px;
-  height: 300px;
-  &:hover {
-    cursor: pointer;
-    box-shadow: 2px 2px 10px #e2e3e9;
-  }
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      padding: 15px;
+      background-color: ${colors.backgroundLight};
+      border-radius: 30px;
+      width: 300px;
+      height: 300px;
+      &:hover {
+            cursor: pointer;
+            box-shadow: 2px 2px 10px #e2e3e9;
+      }
 `
 
 function Card({ label, title, picture }) {
-  return (
-    <CardWrapper>
-      <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="freelance" />
-      <CardTitle>{title}</CardTitle>
-    </CardWrapper>
-  )
+      return (
+            <CardWrapper>
+                  <CardLabel>{label}</CardLabel>
+                  <CardImage src={picture} alt="freelance" />
+                  <CardTitle>{title}</CardTitle>
+            </CardWrapper>
+      )
 }
 
 Card.propTypes = {
-  label: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
 }
 
 Card.defaultProps = {
-  label: '',
-  title: '',
-  picture: DefaultPicture,
+      label: '',
+      title: '',
+      picture: DefaultPicture,
 }
 
 export default Card
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,63 +1,82 @@
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import { useEffect, useEffect } from 'react'
 
 const CardsContainer = styled.div`
-  display: grid;
-  gap: 24px;
-  grid-template-rows: 350px 350px;
-  grid-template-columns: repeat(2, 1fr);
-  align-items: center;
-  justify-items: center;
+    display: grid;
+    gap: 24px;
+    justify-content: center;
+    align-content: center;
+    grid-template-columns: repeat(2, 1fr);
+    width: fit-content;
+    margin: auto;
+    text-align: center;
+    justify-items: center;
+    align-items: center;
 `
 const PageTitle = styled.h1`
-      font-size: 30px;
-      color: black;
-      text-align: center;
-      padding-bottom: 30px;
+    font-size: 30px;
+    color: black;
+    text-align: center;
+    padding-bottom: 30px;
 `
 
 const PageSubtitle = styled.h2`
-      font: 20px;
-      color: ${colors.secondary};
-      font-weight: 300;
-      text-align: center;
-      padding-bottom: 30px;
+    font-size: 20px;
+    color: ${colors.secondary};
+    font-weight: 300;
+    text-align: center;
+    padding-bottom: 30px;
 `
 
 const freelanceProfiles = [
-      {
-            name: 'Jane Doe',
-            jobTitle: 'Devops',
-      },
-      {
-            name: 'John Doe',
-            jobTitle: 'Developpeur frontend',
-      },
-      {
-            name: 'Jeanne Biche',
-            jobTitle: 'Développeuse Fullstack',
-      },
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+    },
 ]
 
 function Freelances() {
-      return (
-            <div>
-                  <PageTitle>Trouvez votre prestataire</PageTitle>
-                  <PageSubtitle>
-                        Chez shiny nous réunissons les meilleurs profils pour vous.
-                  </PageSubtitle>
-                  <CardsContainer>
-                        {freelanceProfiles.map((profile, index) => (
-                              <Card
-                                    key={`${profile.name}-${index}`}
-                                    label={profile.jobTitle}
-                                    title={profile.name}
-                              />
-                        ))}
-                  </CardsContainer>
-            </div>
-      )
+    useEffect(() => {
+        async function fetchFreelances() {
+            try {
+                const response = await fetch(`http://localhost:8000/survey`)
+            } catch {
+            } finally {
+            }
+        }
+        fetchFreelances()
+    }, [])
+    if (error) {
+        return <span>Oups il y a un problème</span>
+    }
+
+    return (
+        <div>
+            <PageTitle>Trouvez votre prestataire</PageTitle>
+            <PageSubtitle>
+                Chez shiny nous réunissons les meilleurs profils pour vous.
+            </PageSubtitle>
+            <CardsContainer>
+                {freelanceProfiles.map((profile, index) => (
+                    <Card
+                        key={`${profile.name}-${index}`}
+                        label={profile.jobTitle}
+                        title={profile.name}
+                    />
+                ))}
+            </CardsContainer>
+        </div>
+    )
 }
 
 export default Freelances
