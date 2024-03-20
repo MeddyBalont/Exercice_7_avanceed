1809dcd600872eb9e1a0c498b65db2f220c584e7
MeddyBalont <Meddy.Balont@hotmail.com>
2024-03-20 09:51:42 +0100

    Error (ThemeProvider)

--- src/index.jsx
+++ src/index.jsx
@@ -7,20 +7,29 @@ import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
-
+import Footer from './components/Footer'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider } from './utils/context'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/survey/:questionNumber" element={<Survey />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/freelances" element={<Freelances />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <ThemeProvider>
+                <GlobalStyle />
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/survey/:questionNumber"
+                        element={<Survey />}
+                    />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/freelances" element={<Freelances />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </ThemeProvider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
