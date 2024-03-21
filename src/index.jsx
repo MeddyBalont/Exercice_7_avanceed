ec212b3b4f7a7a500e867501fd2fc8e6a61d117d
MeddyBalont <Meddy.Balont@hotmail.com>
2024-03-21 12:43:25 +0100

    Dark Mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
-import Error from './components/Error'
 import Footer from './components/Footer'
+import Error from './components/Error'
 import GlobalStyle from './utils/style/GlobalStyle'
-import { ThemeProvider } from './utils/context'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <ThemeProvider>
-                <GlobalStyle />
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/survey/:questionNumber"
-                        element={<Survey />}
-                    />
-                    <Route path="/results" element={<Results />} />
-                    <Route path="/freelances" element={<Freelances />} />
-                    <Route path="*" element={<Error />} />
-                </Routes>
-                <Footer />
+                <SurveyProvider>
+                    <GlobalStyle />
+                    <Header />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/survey/:questionNumber">
+                            <Survey />
+                        </Route>
+                        <Route path="/results">
+                            <Results />
+                        </Route>
+                        <Route path="/freelances">
+                            <Freelances />
+                        </Route>
+                        <Route>
+                            <Error />
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </SurveyProvider>
             </ThemeProvider>
         </Router>
     </React.StrictMode>,
